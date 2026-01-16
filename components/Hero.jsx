09b45da2a3324efdc2c247bcfdbd29c6a3a7acc4
Josue L <josuelobbo@gmail.com>
2026-01-15 21:05:04 -0600

    feat: optimización de navbar, eliminación de galería e integración de blog mdx

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -18,12 +18,10 @@ export default function Hero({ h1, p, bold, blog }) {
 
         {blog ? null : (
           <Link
-            href="https://www.canva.com/design/DAEn3igpKRE/0lKgX5kNk_EFlsJ52ql6NQ/view?utm_content=DAEn3igpKRE&utm_campaign=designshare&utm_medium=link&utm_source=sharebutton"
+            href="https://docs.google.com/document/d/1BxwhWCDy8ZIo3jtLvTNKgrHle-bL9RN92svSJz9xWfI/edit?usp=sharing"
             about="_blank"
           >
-
             <button>Ver CV</button>
-
           </Link>
         )}
       </div>
